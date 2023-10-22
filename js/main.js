192c48cf1bd68d4c25d9b24a32cb8f4dcb13240e
Ilya zrilov <87196340+deathwing122@users.noreply.github.com>
2023-10-22 23:26:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+
+});
