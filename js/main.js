c7d6d76b597a5ee943d8d333d5b57c48897466b6
Ilya zrilov <87196340+deathwing122@users.noreply.github.com>
2023-10-26 18:41:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+const swiper = new Swiper('.hotel__swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
 
-  navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
-  },
+	navigation: {
+		nextEl: '.hotel-slider__button--next',
+		prevEl: '.hotel-slider__button--prev',
+	},
+})
 
-});
+const swiperRew = new Swiper('.reviews__swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+
+	navigation: {
+		nextEl: '.reviews-slider__button--next',
+		prevEl: '.reviews-slider__button--prev',
+	},
+})
